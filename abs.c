9d88d3631b07a07c8d7209f8306dc9623ed595bf
learn-c-daily <learn.c.daily@gmail.com>
2006-11-28 08:28:17 -0800

    take user input

--- abs.c
+++ abs.c
@@ -4,13 +4,18 @@
  * step 2: try printing a number 
  * step 3: print a negative number
  * step 4: check if the number is negative, multiple by -1
+ * step 5: input number from user
  */
 
 #include <stdio.h>
 
 int main()
 {
-   int n = -10;
+   //int n = -10;
+   int n;
+
+   printf("enter any number\n");
+   scanf("%d", &n);
    
    //printf("Hello World\n");
    printf("%d\n", n);
