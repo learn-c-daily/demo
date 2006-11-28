6bc34c1823d08cd5566ac1193af3bad9698c9354
learn-c-daily <learn.c.daily@gmail.com>
2006-11-28 08:23:24 -0800

    print a number

--- abs.c
+++ abs.c
@@ -1,13 +1,17 @@
 /* Problem Statement: Write a program to calculate abs of a number
  *
  * step 1: check hello world program 
+ * step 2: try printing a number 
  */
 
 #include <stdio.h>
 
 int main()
 {
-   printf("Hello World\n");
+   int n = 10;
+   
+   //printf("Hello World\n");
+   printf("%d\n", n);
 
    return 0;
 }
