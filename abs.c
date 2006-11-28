e75a2765bf353a309b6fdb3a127ac04c6972da50
learn-c-daily <learn.c.daily@gmail.com>
2006-11-28 08:26:48 -0800

    fix negative number

--- abs.c
+++ abs.c
@@ -3,6 +3,7 @@
  * step 1: check hello world program 
  * step 2: try printing a number 
  * step 3: print a negative number
+ * step 4: check if the number is negative, multiple by -1
  */
 
 #include <stdio.h>
@@ -12,6 +13,10 @@ int main()
    int n = -10;
    
    //printf("Hello World\n");
+   printf("%d\n", n);
+
+   if(n < 0)
+      n = n * -1;
    printf("%d\n", n);
 
    return 0;
